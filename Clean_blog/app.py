0f9f940dde72ba1b34996300c629a1931c7572d2
Jhoel Perez Ruiz <jhoel.jjpr@gmail.com>
2022-12-19 18:04:54 +0300

    init commit flask website

---
+++ Clean_blog/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, url_for
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    url_for('static', filename='css/style.css')
