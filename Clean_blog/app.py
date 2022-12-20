34b0367898d244d20eab0952b1a6c90ea914a0f1
Jhoel Perez Ruiz <jhoel.jjpr@gmail.com>
2022-12-20 15:27:02 +0300

    modular html and post rendering with jinja

--- Clean_blog/app.py
+++ Clean_blog/app.py
@@ -1,11 +1,36 @@
+#Flask modules
 from flask import Flask, url_for
-from flask import render_template
+from flask import render_template,redirect
+
+#API modules
+import requests 
+import json 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    data = api()
+    return render_template("index.html",all_posts=data)
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    data = api()[post_id-1]
+    return render_template('post.html',post=data)
+
+def api():
+    endpoint ="https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(url=endpoint)
+    data = response.json()
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True)
